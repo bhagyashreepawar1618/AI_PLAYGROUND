cd94cfb300cbb0cf197315b988a63337e3707463
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-08 23:44:01 +0530

    cretaed function to connect database

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import DB_NAME from '../constants.js';
 export const connect_databse = async () => {
   try {
-    await mongoose.connect(`${DB_NAME}/${process.env.DATABASE_URL}`);
+    await mongoose.connect(`${process.env.DATABASE_URL}/${DB_NAME}`);
     console.log('successfully connected to mongoDB!!');
   } catch (error) {
     console.log('Error occureed while connecting to database', error);
