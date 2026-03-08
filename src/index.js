9efaa48dd1fc3cbc0e44ca1777cf7f409a1f9d79
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-08 23:44:17 +0530

    connected to mongodb database

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import app from "./app.js";
-import dotenv from "dotenv";
-import { connect_databse } from "./controllers/index.js";
+import app from './app.js';
+import dotenv from 'dotenv';
+import { connect_databse } from './database/index.js';
 dotenv.config();
 
-
-connect_databse().then(()=>{
+connect_databse()
+  .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-  console.log(`server is listening to port ${process.env.PORT}`);
-});
-}).catch(error){
-    console.log("error occured while connecting to database",error)
-}
-
+      console.log(`server is listening to port ${process.env.PORT}`);
+    });
+  })
+  .catch(error => {
+    console.log('Error occured while connecting to MongoDB', error);
+  });
