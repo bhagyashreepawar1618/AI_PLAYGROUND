599402918017bbbabffad46fd56a9cbbb0630168
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-11 00:00:19 +0530

    added userschema

---
+++ src/models/user.model.js
@@ -0,0 +1,63 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const userSchema = new Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    profilePicture: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.IsPasswordCorrect = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.pre('save', async function () {
+    if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.generateAccessToken =function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            username :this.username,
+            email:this.eamil,
+            fullname:this.fullname
+        },
+        {
+            process.env.ACCESS_TOKEN_SECRET,
+        },
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    )
+}
+export const User = mongoose.model('User', userSchema);
