f90a503143d557222cb2d79032f7e15980e477ca
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-14 00:01:07 +0530

    added method to access req.body

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
+
+app.use(
+  express.json({
+    limit: '10mb',
+  })
+);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: '10mb',
+  })
+);
+
+//to store images and files in public
+app.use(express.static('public'));
 
 export default app;
