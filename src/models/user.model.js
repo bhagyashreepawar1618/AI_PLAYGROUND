afecd2d401610dc258d0fcbf2190ea112ee62573
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-13 23:12:51 +0530

    added generate access and refresh token secret methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -40,24 +40,37 @@ userSchema.methods.IsPasswordCorrect = function (password) {
 };
 
 userSchema.pre('save', async function () {
-    if (!this.isModified("password")) return;
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
 });
 
-userSchema.methods.generateAccessToken =function(){
-    return jwt.sign(
-        {
-            _id:this._id,
-            username :this.username,
-            email:this.eamil,
-            fullname:this.fullname
-        },
-        {
-            process.env.ACCESS_TOKEN_SECRET,
-        },
-        {
-            expiresIn : process.env.ACCESS_TOKEN_EXPIRY,
-        }
-    )
-}
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.eamil,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshTokens = () => {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullname: this.fullname,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 export const User = mongoose.model('User', userSchema);
