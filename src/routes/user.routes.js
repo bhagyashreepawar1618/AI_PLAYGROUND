fef3a6c065c66cb30fda454d10321a9ad3281603
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-14 00:22:29 +0530

    added register user route

---
+++ src/routes/user.routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { registerUser } from '../controllers/user.controller.js';
+import { upload } from '../middleares/multer.middleware.js';
+
+const router = Router();
+
+router.route('/register-user').post(
+  upload.fields([
+    {
+      name: 'profilePicture',
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
