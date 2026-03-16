a727674e0e936cbd11bb4ba51d7a94cd6d24fdb8
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-16 18:30:45 +0530

    updated routing for login,register and home

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,11 +2,33 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/MainComponents/Layout.jsx';
+import Home from './components/MainComponents/Home.jsx';
+import Profile from './components/User/Profile.jsx';
+import Register from './components/MainComponents/Register.jsx';
+import Login from './components/MainComponents/Login.jsx';
 
 const route = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+    ],
   },
 ]);
 
