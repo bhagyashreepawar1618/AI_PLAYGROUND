7741490915d2d9d246d51890f0b9d102f2174b77
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-25 23:19:38 +0530

    added cors and cloudinary configuration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: './.env',
+});
 
 const app = express();
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    credentials: true,
+  })
+);
 
 app.use(
   express.json({
